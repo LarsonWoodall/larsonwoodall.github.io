2326234b726721d1457ee690e599117d68248b0e
Larson Woodall <larsonwoodall@gmail.com>
2018-05-09 11:51:13 -0500

    created skelly

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<html lang="en">
+<head>
+    <title> Larson Woodall </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+Larson Woodall
+</body>
+</html>
