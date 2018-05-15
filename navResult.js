f7a504aef7e94ca7f8b12d340305796dc1f45f74
Larson Woodall <larsonwoodall@gmail.com>
2018-05-15 01:10:56 -0500

    jquery option

--- navResult.js
+++ navResult.js
@@ -9,7 +9,7 @@ function link_events() {
 
 function me() {
     var outarea = document.getElementById("outarea");
-    outarea.innerHTML = "<object type='text/html' data='mystuff.html' ></object>";
+    outarea.load("aboutme.html");
     resetSidebarButtonClasses ();
     var me = document.getElementById("me");
     me.setAttribute("class", "sideBarbuttonSelected");
